2559aff6d803f85364122edd36dc82fc38d4d9eb
kieraliz <kenavarro@ucsd.edu>
2024-05-04 23:01:53 -0700

    Update expose.js -- display image
    Remove RESULT in expose.html when done testing

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,29 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const horn = document.getElementById("horn-select");
+  const result = document.querySelector(".result");  
+  
+  horn.addEventListener("change", changeImage);
+
+  function changeImage(e) {
+    // selects the main image, reads choice
+    const mainImage = document.querySelector('#expose img');
+    const choice = e.target.value;
+
+    // changes main image's src depending on choice
+    if (choice == "air-horn") {
+      mainImage.src = "assets/images/air-horn.svg";
+    }
+    else if (choice == "car-horn") {
+      mainImage.src = "assets/images/car-horn.svg";
+    }
+    else if (choice == "party-horn") {
+      mainImage.src = "assets/images/party-horn.svg";
+    }
+    else {
+      mainImage.src = "assets/images/no-image.png";
+    }    
+  }
+
 }
